42ab64b90655aae3223e4d92a6d723eb34e85d9a
Igor Snisar <snisar.online@gmail.com>
2021-11-22 17:06:10 +0200

    finish game

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,3 +1,4 @@
+'use strict';
 //================================================================//
 //********** Стартовые данные для базы ***************************//
 //================================================================//
@@ -10,7 +11,7 @@ window.onload = function () {
         name: 'Default',
       },
       {
-        delay: 2010,
+        delay: 1800,
         field: 5,
         name: 'easyMode',
       },
@@ -20,27 +21,21 @@ window.onload = function () {
         name: 'normalMode',
       },
       {
-        delay: 900,
+        delay: 800,
         field: 15,
         name: 'hardMode',
       },
       {
-        delay: 900,
+        delay: 650,
         field: 20,
         name: 'superHardMode',
       }
-    ],
-    winners: [
-      {name: 'name1', time: 'time1'},
-      {name: 'name2', time: 'time2'},
-      {name: 'name3', time: 'time3'},
     ]
   };
   localStorage.setItem('globalOptions', JSON.stringify(base));
 
   initOptionsList();
   initBoard(0);
-  initWinnersList$();
 };
 
 
@@ -48,7 +43,7 @@ window.onload = function () {
 //********** Ui-Ui ***********************************************//
 //================================================================//
 function initOptionsList() {
-  const levelList = document.getElementById('List_difficulty');
+  const levelList = document.getElementById('difficultyList');
   const BASE = JSON.parse(localStorage.getItem('globalOptions'));
   let htmlList = ``;
 
@@ -60,11 +55,11 @@ function initOptionsList() {
 
 function initBoard(selectedLevel = 0) {
   const BASE = JSON.parse(localStorage.getItem('globalOptions'));
-  const board = document.getElementById('Board');
+  const board = document.getElementById('boardId');
   const countSquares = BASE.mode[selectedLevel].field * 3;
 
   let htmlList = ``;
-  for (let i = countSquares; i >= 0; i--) {
+  for (let i = countSquares; i > 0; i--) {
     htmlList += `<div class="board__item"></div>`;
   }
 
@@ -89,38 +84,17 @@ function initBoard(selectedLevel = 0) {
 
 function onChangBoard(event) {
   const selectElement = event.target;
-  this.initBoard(selectElement.value);
+  initBoard(selectElement.value);
 }
-
-
-function initWinnersList$() {
-  const winnersList = document.getElementById('winnersList');
-  const BASE = JSON.parse(localStorage.getItem('globalOptions'));
-  const countWinners = BASE.winners.length;
-
-  let htmlList = ``;
-  for (let i = 0; i < countWinners; i++) {
-    htmlList += `<div>
-            <span>${i}</span>
-            <span>${BASE.winners[i].name}</span>
-            <span>${BASE.winners[i].time}</span>
-          </div>`;
-  }
-
-  winnersList.innerHTML = htmlList;
-}
-
-// todo: добавление
-function addNewPlayer() {}
-
-function saveWinner() {}
 
 
 //================================================================//
-//********** Логика **********************************************//
+//********** Логика самой игры ***********************************//
 //================================================================//
 /**
  * Start game
+ * Данная реализация имеет много замечаний в самой реализации
+ * и непотдерживает остановку и паузу.
  */
 function onStartGame(event) {
   /* // v2 example
@@ -131,48 +105,64 @@ function onStartGame(event) {
   event.preventDefault();
   const lv = event.target[1].value; // выбранный индекс/позиция настройки
   const userName = event.target[3].value; // введенное имя
+  const board = document.getElementById('boardId');
   const BASE = JSON.parse(localStorage.getItem('globalOptions'));
   const selectedLevel = BASE.mode[lv]; // выберем нужные параметры по индексу из массива
-  const setting = Object.assign({userName}, selectedLevel); // обьеденяем данные в один обьект
-  console.log('setting', setting);
+  const setting = Object.assign({userName}, selectedLevel); // обьеденяем данные в один обьект для удобства
+  const randomSquaresArr = randomSquares(setting); // получим массив с рандомными цифрами в заданном количестве.
+  const winSquares = [];
+  const losingSquares = [];
 
-  const randomSquaresArr = this.randomSquares(setting);
-  const settingDelay = setting.delay;
+  // запуск игры
+  for (let i = 0; i < randomSquaresArr.length; i++) {
+    setTimeout(() => { // индивидуальная задержка для каждого квадрата
+      const numberSquares = randomSquaresArr[i];
 
-  // todo: закончить катку
-  // start random activation
-  // from(randomSquaresArr)
-  //   .pipe(
-  //     concatMap(x => of(x)
-  //       .pipe(
-  //         delay(settingDelay)
-  //       )
-  //     ),
-  //     takeUntil(this.destroyStream$)
-  //   )
-  //   .subscribe((id) => {
-  //     this.toggleActiveSquares(id);
-  //
-  //     setTimeout(() => {
-  //       this.toggleActiveSquares(id);
-  //
-  //       this.userDontClickSquare(id);
-  //
-  //       this.endGame(randomSquaresArr, id);
-  //
-  //       this.cdr.detectChanges();
-  //     }, settingDelay);
-  //   });
+      const currentSquares = board.querySelector(`div:nth-child(${numberSquares + 1})`); // находим нужный квадрат
+      let isActive = false; // флаг успешного нажатия
+
+      // собитие удачного нажатия
+      const listenActivator = (event) => {
+        isActive = true;
+        currentSquares.classList.add('win');
+        winSquares.push(numberSquares); // добавим к выигранным
+      };
+
+      currentSquares.classList.add('active'); // сообщаем игроку что он активный.
+      currentSquares.addEventListener("click", listenActivator); // подслушаем событие
+
+      // установим таймер за время которого игрок должен успеть нажать нужный квадрат
+      setTimeout(() => {
+        currentSquares.classList.remove('active'); // время вышло - убираем всю активность
+        currentSquares.removeEventListener("click", listenActivator);
+        if (!isActive) { // в случае если игрок не успел нажать за отведенное время на активную кнопку
+          currentSquares.classList.add('losing'); // пометим ее как - проигранную
+          losingSquares.push(numberSquares); // и сохраним в проигранныую
+        }
+      }, setting.delay);
+    }, setting.delay * i);
+  }
+
+  // дождемся когда игра закончится и проверем результат
+  setTimeout(() => checkResultGame(setting, winSquares, losingSquares), setting.delay * randomSquaresArr.length);
+}
+
+function checkResultGame(setting, winSquares, losingSquares) {
+  if (winSquares.length > losingSquares.length) {
+    alert(`${setting.userName} is win`);
+  }
+  else {
+    alert(`${setting.userName} is losing`);
+  }
+  document.location.reload();
 }
 
 //================================================================//
 //********** Вспомагательные *************************************//
 //================================================================//
-function showGameMessage(who) {
-  alert(`${who.winner} is win`);
-}
 
 function randomSquares(setting) {
+  // todo: оптимизировать
   const min = 0;
   const maxNumberSquares = setting.field * 3;
 
